ce86af006bf71f73879b0958a102390939281713
Lord-Raymik <silas.johnson@bsu.edu>
2026-01-20 23:21:53 -0500

    created the SillyMethods class as well as a test class for it. All 4 tests are in working order.

--- src/main/java/edu/bsu/cs/SillyMethods.java
+++ src/main/java/edu/bsu/cs/SillyMethods.java
@@ -1,4 +1,21 @@
 package edu.bsu.cs;
 
 public class SillyMethods {
+
+    public boolean didGuess42(int guess) {
+        if (guess == 42) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String countTo(int amount) {
+        String result = "";
+        for (int i = 0; i < amount; i++) {
+            result += i + " ";
+        }
+        return result += amount;
+    }
+
 }
